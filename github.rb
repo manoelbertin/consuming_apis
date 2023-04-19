cbb456dc9cab7c6c344c6f8f13cec213020ced0b
manoel bertin <manoeltirj@gmail.com>
2023-04-18 21:03:49 -0300

    criei uma variável resp onde passo a montagem da nossa url de busca por repositórios no github.

--- github.rb
+++ github.rb
@@ -2,5 +2,10 @@ require 'rest-client'
 # ferramenta de consumo de API através de chamadas REST
 require 'json'
 
+url = 'https://api.github.com/search/repositories?'
+q = 'spotcode'
+language = 'ruby'
 
+resp = RestClient.get "#{url}q=#{q}&language=#{language}"
+# montei a nossa url que ficou similar ao exemplo no github
 
